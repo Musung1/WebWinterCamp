04d253da5ab79880019ba451d542cee199335d38
musung <musungkim1352@gmail.com>
2024-01-23 20:23:18 +0900

    [Docs] swagger 적용 #8

--- MyBoard/src/main/java/com/example/myboard/dto/TbBoardUpdateFormDTO.java
+++ MyBoard/src/main/java/com/example/myboard/dto/TbBoardUpdateFormDTO.java
@@ -1,6 +1,7 @@
 package com.example.myboard.dto;
 
 import com.example.myboard.domain.TbBoard;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Schema
 public class TbBoardUpdateFormDTO {
+    @Schema(description = "아이디", example="asdfjkleidkdkdkslw")
     private String id;
+    @Schema(description = "제목", example="제목입니다")
     private String title;
+    @Schema(description = "내용", example="내용입니다")
     private String content;
 
 }
